4ddcf37f36a8b0d20d867d937c92832ec914bb42
Rojas Córsico Ivana Soledad <irojas@gmail.com>
2023-12-13 17:58:13 -0300

    custom token with io.jsonwebtoken 0.12.3

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# Spring Boot: Custom Token Project
+
+This project demonstrates how to modify or customize the token in a Spring Boot application..
+
+## Technologies
+  - Spring Boot 3
+  - Spring Security 6
+  - io.jsonwebtoken 0.12.3
+
+## YouTube Channel
+
+   For a detailed step-by-step explanation of the login implementation, please visit https://youtu.be/Yrj10XGGiF4.
+
+
+
---
+++ src/main/java/com/irojas/demojwt/Jwt/JwtService.java
@@ -0,0 +1,85 @@
+package com.irojas.demojwt.Jwt;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import com.irojas.demojwt.User.User;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
+
+    public String getToken(User user) {
+        return getToken(new HashMap<>(), user);
+    }
+
+    private String getToken(Map<String,Object> extraClaims, User user) {
+        return Jwts
+            .builder()
+            .claims(extraClaims)
+            .claim("userId", user.getId())
+            .claim(("firstName"), user.getFirstname())
+            .claim("lastName", user.getLastname())
+            .subject(user.getUsername())
+            .issuedAt(new Date(System.currentTimeMillis()))
+            .expiration(new Date(System.currentTimeMillis()+1000*60*24))
+            .signWith(getKey())
+            .compact();
+    }
+
+    private SecretKey getKey() {
+       byte[] keyBytes=Decoders.BASE64.decode(SECRET_KEY);
+       return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String getUsernameFromToken(String token) {
+        return getClaim(token, Claims::getSubject);
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username=getUsernameFromToken(token);
+        return (username.equals(userDetails.getUsername())&& !isTokenExpired(token));
+    }
+
+    private Claims getAllClaims(String token)
+    {
+        return Jwts
+            .parser()
+            .verifyWith(getKey())
+            .build()
+            .parseSignedClaims(token)
+            .getPayload();
+    }
+
+    public <T> T getClaim(String token, Function<Claims,T> claimsResolver)
+    {
+        final Claims claims=getAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Date getExpiration(String token)
+    {
+        return getClaim(token, Claims::getExpiration);
+    }
+
+    private boolean isTokenExpired(String token)
+    {
+        return getExpiration(token).before(new Date());
+    }
+    
+}
